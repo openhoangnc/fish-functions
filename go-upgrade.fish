1cbd88228ab659dc5a68055201c97140efbf0b0d
Nguyen Cong Hoang <hoangnc.jp@gmail.com>
2022-03-16 16:23:02 +0700

    Update go-upgrade.fish

--- go-upgrade.fish
+++ go-upgrade.fish
@@ -3,10 +3,10 @@ function go-upgrade
     set curVer (go version | grep -o -E 'go\d+\.\d+\.\d+')
     set arch (go version | grep -o -E '[^ ]+$' | sed 's/\//-/g') 
     echo Current Go version: $curVer
-    set pkgReg "go\\d+\\.\\d+\\.\\d+\\.$arch\\.pkg"
+    set pkgReg "go\\d+\\.\\d+(\\.\\d+)?\\.$arch\\.pkg"
     echo Getting latest Go version...
     set latestPkg (curl -sS https://go.dev/dl/ 2>/dev/null | grep -o -E -m 1 $pkgReg | head -1)
-    set latestVer (echo $latestPkg | grep -o -E 'go\d+\.\d+\.\d+')
+    set latestVer (echo $latestPkg | grep -o -E 'go\d+\.\d+(\.\d+)?')
     if [ "$curVer" = "$latestVer" ]
         echo You have latest Go version
     else
